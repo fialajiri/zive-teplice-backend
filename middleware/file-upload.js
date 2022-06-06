3354f866000e9a0a3562699a49c0598032b9ed22
Jiri Fiala <jirik.fiala@post.cz>
2022-06-06 20:06:16 +0200

    file upload limit change

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -53,7 +53,7 @@ const multerS3Config = (destinationPath) =>
 
 const fileUpload = (destinationPath) =>
   multer({
-    limits: 2000000,
+    limits: 20000000,
     storage: multerS3Config(destinationPath),
     fileFilter: fileFilter,
   });
